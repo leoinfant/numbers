e83581f43f67d921e269ddd0b40f4dcc18822413
leoinfant <leoinfant@users.noreply.github.com>
2017-06-23 15:48:02 +0530

    Add files via upload

---
+++ Fibnocies.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Fibnocies {
+
+	public static void main(String[] args) {
+		int n1=0;
+		int n2=1;
+		int n3;
+		Scanner n=new Scanner(System.in);
+		
+		int count=n.nextInt();
+		System.out.println(+n1+n2);
+		for(int i=0;i<count;i++){
+			n3=n1+n2;
+			System.out.println(n3);
+			n1=n2;
+			n2=n3;
+		}
+		
+	}
+
+}
