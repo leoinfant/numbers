4d7d33413861d11203e9021e1a21b2f74089f3c8
leoinfant <leoinfant@users.noreply.github.com>
2017-06-28 10:16:37 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the n value:");
+scanf("%d",&n);
+if(n<=9)
+{
+printf("%d",&n);
+}
+else{
+printf("you entered number is not in range");
+}
+}
